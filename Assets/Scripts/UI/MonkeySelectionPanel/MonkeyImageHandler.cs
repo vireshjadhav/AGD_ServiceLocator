d30486a7292f87b24e80bcff17e36a55d68e8348
Viresh Jadhav <vireshjadhav@gmail.com>
2026-04-24 01:49:48 +0530

    feat: implement drag & drop for monkey image
    1. Added IDragHandler for drag functionality.
    2. Implemented IPointerDownHandler and IDragEndHandler.
    3. Added visual feedback (color change) during drag.
    4. Reset state after drag ends.

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,22 +6,56 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour, IDragHandler, IPointerDownHandler, IEndDragHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
 
+        private RectTransform rectTransform;
+        private Vector3 originalPosition;
+        private Vector3 originalAnchoredPosition;
+
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
+            
             this.spriteToSet = spriteToSet;
             this.owner = owner;
         }
 
         private void Awake()
         {
+            rectTransform = GetComponent<RectTransform>();
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
+            originalPosition = rectTransform.position;
+            originalAnchoredPosition = rectTransform.anchoredPosition;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            rectTransform.anchoredPosition += eventData.delta;
+            owner.MonkeyDraggedAt(rectTransform.position);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            monkeyImage.color = new Color(1, 1, 1, 0.6f);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMonkey();
+            owner.MonkeyDroppedAt(eventData.position);
+        }
+
+        private void ResetMonkey()
+        {
+            monkeyImage.color = new Color(1, 1, 1, 1f);
+            rectTransform.anchoredPosition = originalAnchoredPosition;
+            rectTransform.position = originalPosition;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
         }
     }
 }
